654bdbe22653aaf9838b610eaf00ca55ece096d7
Bjarki Baldvinsson <bjarkib16@ru.is>
2019-12-10 10:55:10 +0000

    little styling on cinemalist

--- src/components/CinemaThumbnail/styles.js
+++ src/components/CinemaThumbnail/styles.js
@@ -3,16 +3,27 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   container: {
     flexDirection: 'row',
-    color: 'blue'
+    color: 'blue',
+    padding: 5,
+    borderWidth: 0.5,
+    borderColor: 'white',
+    backgroundColor: '#bbbfbf',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60
+
   },
 
   cinema: {
-  fontSize: 24
+  fontSize: 20,
+  flex: 1
 },
 
   link: {
+    flex: 1,
     fontStyle: 'italic',
-    fontSize: 24,
-    textDecorationLine: 'underline'
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: 'blue'
   }
 })
