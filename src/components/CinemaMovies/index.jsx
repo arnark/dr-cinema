2a52812fe31bbae71d149919d8071eae16f8808f
Arnarkj <arnark18@ru.is>
2019-12-11 20:29:38 +0000

    Some

--- src/components/CinemaMovies/index.jsx
+++ src/components/CinemaMovies/index.jsx
@@ -25,7 +25,7 @@ const CinemaMovies = ({ cinemaMovies, cinemaId, navigation }) => (
           navigation={navigation}
         />
       )}
-      keyExtractor={(_id) => _id.toString()}
+      keyExtractor={(title) => title.toString()}
     />
   </View>
 );
